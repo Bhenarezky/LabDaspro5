0750c804faeb851b2690876e922dd9280ee70d49
Bhenarezky <145680874+Bhenarezky@users.noreply.github.com>
2023-09-24 21:49:10 +0700

    Update kuis_1_B_Bhenarezky_003.pas

--- kuis_1_B_Bhenarezky_003.pas
+++ kuis_1_B_Bhenarezky_003.pas
@@ -1,15 +1,15 @@
 uses crt;
 var
-a,c:string;
-b,d:integer;
+  a,c:string;
+  b,d:integer;
 begin
-writeln ('masukkan jalan :');
-readln(a);
-writeln('masukkan No :');
-readln(b);
-writeln('masukkan Kota :');
-readln(c);
-writeln('masukkan kode pos :');
-readln(d);
-writeln('Alamat : Jalan ',a,' No. ', b ,' ', c ,' ', d );
+  writeln ('masukkan jalan :');
+  readln(a);
+  writeln('masukkan No :');
+  readln(b);
+  writeln('masukkan Kota :');
+  readln(c);
+  writeln('masukkan kode pos :');
+  readln(d);
+  writeln('Alamat : Jalan ',a,' No. ', b ,' ', c ,' ', d );
 end.
