bce2253a074e8b97da46c375e89eaa02c1347374
Bhenarezky <145680874+Bhenarezky@users.noreply.github.com>
2023-09-24 20:51:46 +0700

    Create alamat.pas

---
+++ alamat.pas
@@ -0,0 +1,15 @@
+uses crt;
+var
+a,c,:string;
+b,d:integer;
+begin
+writeln ('masukkan jalan :');
+readln(a);
+writeln('masukkan No :');
+readln(b);
+writeln('masukkan Kota :');
+readln(c);
+writeln('masukkan kode pos :');
+readln(d);
+writeln('Alamat : Jalan ',a,' No. ',b ,c ,d );
+end.
