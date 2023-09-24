3828ee6280b1657e33b62693e31d396b3f3be558
Bhenarezky <145680874+Bhenarezky@users.noreply.github.com>
2023-09-24 21:48:15 +0700

    Update kuis_1_A_Bhenarezky_003.pas

--- kuis_1_A_Bhenarezky_003.pas
+++ kuis_1_A_Bhenarezky_003.pas
@@ -1,15 +1,15 @@
 uses crt;
 var
-a,c,d:string;
+ a,c,d:string;
 b:longint;
 begin
-writeln ('masukkan nama anda :');
-readln(a);
-writeln('masukkan NIM anda :');
-readln(b);
-writeln('masukkan KOM anda :');
-readln(c);
-writeln('masukkan alamat anda :');
-readln(d);
-writeln('Hallo, Nama saya ',a,', NIM saya ',b,',saya di KOM ',C,' Alamat saya di jalan ',d,'.Salam Kenal Semua.')
+ writeln ('masukkan nama anda :');
+ readln(a);
+ writeln('masukkan NIM anda :');
+ readln(b);
+ writeln('masukkan KOM anda :');
+ readln(c);
+ writeln('masukkan alamat anda :');
+ readln(d);
+ writeln('Hallo, Nama saya ',a,', NIM saya ',b,',saya di KOM ',C,' Alamat saya di jalan ',d,'.Salam Kenal Semua.')
 end.
